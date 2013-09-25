57793b17ec5beeaa68cc0deec18143e835bc2fc4
arau <arau@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-25 10:01:40 +0000

    Deleting HTSFilter from R-Forge (current and development versions available on Bioconductor
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/htsfilter@59 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/HTSFilter/R/AllGenerics.R
+++
@@ -1,9 +0,0 @@
-setGeneric(
-		name = "HTSFilter",
-		def = function(x,...) {standardGeneric("HTSFilter")}
-		)
-
-setGeneric(
-		name = "HTSBasicFilter",
-		def = function(x,...) {standardGeneric("HTSBasicFilter")}
-		)
--- pkg/HTSFilter/vignettes/Compile_HTSFilter.R
+++
@@ -1,5 +0,0 @@
-setwd("C:/Users/Andrea/Desktop/HTSFilter_dev/vignettes")
-## install.packages("SweaveListingUtils")
-library(SweaveListingUtils)
-Sweave("HTSFilter.Rnw")
-
